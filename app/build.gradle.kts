ec1e5222e866aa3f3b4c0b60bfe6b638dad138d5
Madhur Milind Jaripatke <84721727+MadhurJ20@users.noreply.github.com>
2023-07-23 00:28:55 +0530

    Added Firebase Analytics
    Signed-off-by: Madhur Milind Jaripatke <84721727+MadhurJ20@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services") version "4.3.15" apply false
 }
 
 android {
@@ -33,6 +34,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.2.0"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
